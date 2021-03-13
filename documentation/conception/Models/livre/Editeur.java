55c8ea01c8b8856df0032fe9f07bdd701bc88e5f
aquel69 <alexandre.lardon@yahoo.fr>
2021-03-13 09:21:23 +0100

    mise à jour des spécifications fonctionnelles terminé

--- documentation/conception/Models/livre/Editeur.java
+++
@@ -1,7 +0,0 @@
-package com.lardon.projet7bibliotheque.model.livre;
-
-public class Editeur {
-    private int idEditeur;
-    private String nomMaisonEdition;
-
-}
--- documentation/conception/Models/referentiel/EnumRole.java
+++
@@ -1,7 +0,0 @@
-package com.lardon.projet7bibliotheque.model.referentiel;
-
-public class EnumRole {
-    private int code;
-    private String status;
-    private String description;
-}
